a51b2e36884998282b445d2ccfc92a79cfb35423
AdrianKas <66158096+AdrianKas@users.noreply.github.com>
2020-10-21 19:26:31 +0200

    Update constants.py

--- bothunting/core/notebooks/constants.py
+++ bothunting/core/notebooks/constants.py
@@ -1,8 +1,3 @@
-CONSUMER_KEY = "2dqM1oxHL6ybNsSfMgdwGf2iO"
-CONSUMER_SECRET = "sYKbwIC24d9RKtZW5CXaxK8t8Q5fMvG1hkkYtV4At9egh9Fdd4"
-ACCESS_TOKEN = "1898213802-g9kTPGb720zaVjHeA73APcn8NwsDrPjCnh9qbf6"
-ACCESS_TOKEN_SECRET = "8lu2Rmhzmve4GtzVvWNXVM2nZCrzA1g3BTpsIj8HvTUOf"
-
 TEST_SET = [
     "DanieleMaraldi",
     "GianlucaPriopi",
